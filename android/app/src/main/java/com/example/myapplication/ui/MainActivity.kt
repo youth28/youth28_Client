13ba60fc9199eefa443c3ace545ff906c8da6323
ansuvin <dkstnqls0925@naver.com>
2021-01-20 14:26:11 +0900

    [UPDATE] MainActivity

---
+++ android/app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMenu.setOnClickListener {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawers()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
