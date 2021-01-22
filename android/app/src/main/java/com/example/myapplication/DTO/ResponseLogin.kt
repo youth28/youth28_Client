2ade54d37ab6419e3cadd578d0a51600b37ed1f7
ansuvin <dkstnqls0925@naver.com>
2021-01-22 17:08:28 +0900

    [UPDATE] Response Login

--- android/app/src/main/java/com/example/myapplication/DTO/ResponseLogin.kt
+++ android/app/src/main/java/com/example/myapplication/DTO/ResponseLogin.kt
@@ -2,11 +2,16 @@ package com.example.myapplication.DTO
 
 import com.google.gson.annotations.SerializedName
 
-class ResponseLogin {
-    @SerializedName("name")
-    private var name =""
-    @SerializedName("profile")
-    private var profile =""
+data class ResponseLogin(
+        val user_id: String,
+        val name: String,
+        val profile: String
+){
+
+
+    override fun toString(): String {
+        return "ResponseLogin(name='$name', profile='$profile', userId=$user_id)"
+    }
 
 
 }
