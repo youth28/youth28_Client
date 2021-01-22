13d3e7e3589e621425d41315c0319ac4703bd41a
ansuvin <dkstnqls0925@naver.com>
2021-01-22 17:13:12 +0900

    [ADD] UserAPI - modify

--- android/app/src/main/java/com/example/myapplication/API/UserAPI.kt
+++ android/app/src/main/java/com/example/myapplication/API/UserAPI.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.API
 
 import com.example.myapplication.DTO.ResponseLogin
 import com.example.myapplication.DTO.UserDTO
+import com.example.myapplication.DTO.UserModify
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -21,4 +22,9 @@ interface UserAPI {
     fun check_emial(
             @Body userDTO: UserDTO
     ): Call<UserDTO>
+
+    @POST("modify")
+    fun modify(
+            @Body userModify: UserModify
+    ): Call<UserModify>
 }
