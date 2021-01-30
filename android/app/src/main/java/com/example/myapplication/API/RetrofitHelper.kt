702e4b64f85c7548ecc344423005020382e0840f
ansuvin <dkstnqls0925@naver.com>
2021-01-30 23:38:01 +0900

    [UPDATE] RetrofitAPI

--- android/app/src/main/java/com/example/myapplication/API/RetrofitHelper.kt
+++ android/app/src/main/java/com/example/myapplication/API/RetrofitHelper.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
 
-    const val API_URL = "http://9e19b69318d2.ngrok.io/"
+    const val API_URL = "http://b8f98d34e53d.ngrok.io/"
 
     private var instanc: Retrofit? = null
     private val gson = GsonBuilder().setLenient().create()
