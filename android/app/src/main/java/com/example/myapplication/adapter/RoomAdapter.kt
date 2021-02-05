65c57d07b56ceba37a5c152dec789c44cf9bb97e
ansuvin <dkstnqls0925@naver.com>
2021-02-05 16:20:50 +0900

    [MOVE] Adapter

--- android/app/src/main/java/com/example/myapplication/adapter/RoomAdapter.kt
+++ android/app/src/main/java/com/example/myapplication/adapter/RoomAdapter.kt
@@ -1,26 +1,24 @@
-package com.example.myapplication
+package com.example.myapplication.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.RoomModel
 import com.example.myapplication.databinding.RowRoomBinding
 import com.example.myapplication.dialog.JoinDialog
 
 class RoomAdapter(val context: Context): RecyclerView.Adapter<RoomAdapter.Holder>() {
     var list = listOf<RoomModel>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomAdapter.Holder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = RowRoomBinding.inflate(LayoutInflater.from(context), parent, false)
 
         return Holder(binding)
     }
 
-    override fun onBindViewHolder(holder: RoomAdapter.Holder, position: Int) {
+    override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.onBind(list = list[position])
 
         val mAdapter = TagAdapter(context)
