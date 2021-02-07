8a55f046c573b04d99e84295addea327846cbc37
ansuvin <dkstnqls0925@naver.com>
2021-02-08 02:46:26 +0900

    [UPDATE] DataBinding - LoginActivity

--- android/app/src/main/java/com/example/myapplication/ui/LoginActivity.kt
+++ android/app/src/main/java/com/example/myapplication/ui/LoginActivity.kt
@@ -8,10 +8,12 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.myapplication.API.RetrofitHelper
 import com.example.myapplication.DTO.ResponseLogin
 import com.example.myapplication.DTO.UserDTO
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityLoginBinding
 import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,25 +23,34 @@ class LoginActivity: AppCompatActivity() {
 
     val TAG = "LoginActivity"
 
-    var strId = ""
-    var strPW = ""
+    private lateinit var binding: ActivityLoginBinding
+
+    var email = ""
+    var PW = ""
+    var errEmail = ""
+    var errPW = ""
 
     internal lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.activity = this
 
         preferences = getSharedPreferences("user", Activity.MODE_PRIVATE)
-        strId = preferences.getString("userId", "null").toString()
-        strPW = preferences.getString("userPW", "null").toString()
+        email = preferences.getString("userId", "").toString()
+        PW = preferences.getString("userPW", "").toString()
 
-        Log.e(TAG, "$strId, $strPW")
+        Log.e(TAG, "$email, $PW")
 
-        // strId, strPw 값이 null이 아니라면 자동 로그인 하기
-        if (strId !="null" && strPW !="null"){
+        // email, PW 값이 null이 아니라면 자동 로그인 하기
+        autoLogin()
+    }
+
+    fun autoLogin() {
+        if (email !="null" && PW !="null"){
             val user = getData()
-            Log.e(TAG, "$strId  $strPW")
+            Log.e(TAG, "$email  $PW")
             val call = RetrofitHelper.getApiService().login(user)
             Log.e(TAG, user.toString())
             call.enqueue(object : Callback<ResponseLogin> {
@@ -58,8 +69,8 @@ class LoginActivity: AppCompatActivity() {
                                 finish()
                             }
                             204 -> {
-                                tvErrPw.text = "아이디나 비밀번호가 일치하지 않습니다."
-                                tvErrPw.visibility = View.VISIBLE
+                                errPW = "아이디나 비밀번호가 일치하지 않습니다."
+                                binding.tvErrPw.visibility = View.VISIBLE
                             }
                         }
                     }
@@ -71,89 +82,80 @@ class LoginActivity: AppCompatActivity() {
 
             })
         }
+    }
 
-        btnLogin.setOnClickListener {
-            strId = editEmail.text.toString()
-            strPW = editPW.text.toString()
+    fun onSignup(view: View) {
+        val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+        startActivity(intent)
+    }
 
-            if (strId == "" || strPW ==""){
-                if (strId == "") {
-                    tvErrId.visibility = View.VISIBLE
-                    tvErrId.text = "Email을 작성해주세요"
-                } else {
-                    tvErrId.visibility = View.GONE
-                }
-                if (strPW == ""){
-                    tvErrPw.visibility = View.VISIBLE
-                    tvErrPw.text = "비밀번호를 작성해주세요"
-                } else {
-                    tvErrPw.visibility = View.GONE
-                }
+    fun onLogin(view: View) {
+        if (email == "" || PW ==""){
+            if (email == "") {
+                tvErrId.visibility = View.VISIBLE
+                errEmail = "Email을 작성해주세요"
             } else {
                 tvErrId.visibility = View.GONE
+            }
+            if (PW == ""){
+                tvErrPw.visibility = View.VISIBLE
+                errPW = "비밀번호를 작성해주세요"
+            } else {
                 tvErrPw.visibility = View.GONE
+            }
+        } else {
+            tvErrId.visibility = View.GONE
+            tvErrPw.visibility = View.GONE
 
-                // 로그인 통신 코드
-                val user = getData()
-                val call = RetrofitHelper.getApiService().login(user)
-                Log.e(TAG, user.toString())
-                call.enqueue(object : Callback<ResponseLogin> {
-                    override fun onResponse(call: Call<ResponseLogin>, response: Response<ResponseLogin>) {
-                        if (response.isSuccessful) {
-                            val result = response.code()
-                            when (result) {
-                                200 -> {
-                                    //성공할시
-                                    showToast("자동 로그인 등록")
+            // 로그인 통신 코드
+            val user = getData()
+            val call = RetrofitHelper.getApiService().login(user)
+            Log.e(TAG, user.toString())
+            call.enqueue(object : Callback<ResponseLogin> {
+                override fun onResponse(call: Call<ResponseLogin>, response: Response<ResponseLogin>) {
+                    if (response.isSuccessful) {
+                        val result = response.code()
+                        when (result) {
+                            200 -> {
+                                //성공할시
+                                showToast("자동 로그인 등록")
 
-                                    preferences = getSharedPreferences("user", Activity.MODE_PRIVATE)
-                                    val editor = preferences.edit()
-                                    editor.putString("userId", strId)
-                                    editor.putString("userPW", strPW)
-                                    editor.putString("userNum", response.body()!!.user_id)
-                                    Log.e(TAG+" userNum", response.body()!!.user_id)
-                                    editor.putString("userName", response.body()!!.name)
-                                    editor.putString("userProfile", "image")
-                                    editor.apply()
+                                preferences = getSharedPreferences("user", Activity.MODE_PRIVATE)
+                                val editor = preferences.edit()
+                                editor.putString("userId", email)
+                                editor.putString("userPW", PW)
+                                editor.putString("userNum", response.body()!!.user_id)
+                                Log.e(TAG+" userNum", response.body()!!.user_id)
+                                editor.putString("userName", response.body()!!.name)
+                                editor.putString("userProfile", "image")
+                                editor.apply()
 
-                                    Log.e(TAG+" Response", response.body().toString())
+                                Log.e(TAG+" Response", response.body().toString())
 
-                                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                                    startActivity(intent)
-                                    finish()
-                                }
-                                204 -> {
-                                    tvErrPw.text = "아이디나 비밀번호가 일치하지 않습니다."
-                                    tvErrPw.visibility = View.VISIBLE
-                                    Log.e(TAG, "204")
-                                }
+                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                            204 -> {
+                                tvErrPw.text = "아이디나 비밀번호가 일치하지 않습니다."
+                                tvErrPw.visibility = View.VISIBLE
+                                Log.e(TAG, "204")
                             }
                         }
                     }
+                }
 
-                    override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
-                        Log.e(TAG+" Err", "통신안됨: ${t.message}")
-                    }
+                override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
+                    Log.e(TAG+" Err", "통신안됨: ${t.message}")
+                }
 
-                })
+            })
 
-            }
-        }
-
-        tvSignup.setOnClickListener {
-            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        // null이 아니면 자동로그인
-        if (strId != null && strPW!= null){
-            // 로그인 통신 코드
-            val user = getData()
         }
     }
 
     fun getData(): UserDTO {
-        val data = UserDTO(strId, strPW)
+        val data = UserDTO(email, PW)
         return data
     }
 
