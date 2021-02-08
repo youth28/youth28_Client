81c2c256f087c3e978ae4ed2446b64486e153b74
ansuvin <dkstnqls0925@naver.com>
2021-02-08 17:31:30 +0900

    [UPDATE} DataBinding - RoomFindActivity

--- android/app/src/main/java/com/example/myapplication/ui/RoomFindActivity.kt
+++ android/app/src/main/java/com/example/myapplication/ui/RoomFindActivity.kt
@@ -3,8 +3,9 @@ package com.example.myapplication.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
+import android.view.View
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.API.RetrofitHelper
 import com.example.myapplication.DTO.RoomNameDTO
@@ -12,8 +13,8 @@ import com.example.myapplication.DTO.RoomsDTO
 import com.example.myapplication.R
 import com.example.myapplication.adapter.RoomAdapter
 import com.example.myapplication.RoomModel
+import com.example.myapplication.databinding.ActivityRoomFindBinding
 import kotlinx.android.synthetic.main.activity_room_find.*
-import kotlinx.android.synthetic.main.activity_room_find.btnSearch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,83 +23,65 @@ class RoomFindActivity : AppCompatActivity() {
 
     val TAG = "RoomFindActivity"
 
+    private lateinit var binding: ActivityRoomFindBinding
+
     var key = ""
     val list: ArrayList<RoomModel> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_room_find)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_room_find)
+        binding.activity = this
 
         listRecyclerView()
+    }
 
-        val searchList: ArrayList<String> = arrayListOf(
-                "스터디", "업무", "게임", "음악", "미술", "운동(헬스)", "기타"
-        )
+    fun onFind(view: View) {
+        list.clear()
 
-        val adapter = ArrayAdapter<String>(
-                this,
-                R.layout.spinner_item,
-                searchList
-        )
+        if (key == "") {
+            showToast("방 제목을 입력해주세요.")
+        } else {
+            val roomName = RoomNameDTO(key)
+            Log.e(TAG, roomName.toString())
+            val call = RetrofitHelper.getApiService().room_search(roomName)
+            call.enqueue(object : Callback<RoomsDTO> {
+                override fun onResponse(call: Call<RoomsDTO>, response: Response<RoomsDTO>) {
+                    if(response.isSuccessful) {
+                        when (response.code()) {
+                            200 -> {
+                                val result = response.body()
+                                for (i: Int in 1..result!!.room.size) {
+                                    val info = result.room[i-1]
+                                    val field = info.field.substring(0, result.room[i-1].field.length -1)
+                                    val fieldArray = field.split(",")
+                                    list.add(RoomModel(info.room_id, info.title, info.maxPeo,
+                                            fieldArray, info.profile))
+                                    Log.e(TAG, "RoomModel(room_id=${info.room_id}, title='${info.title}', maxPeo=${info.maxPeo}," +
+                                            " field='$fieldArray', profile='${info.profile}')")
 
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
-
-        autoTVRoomFind.setAdapter(adapter)
-
-        autoTVRoomFind.threshold = 1
-
-        autoTVRoomFind.setOnItemClickListener { parent, view, position, id ->
-            showToast("{parent: $parent, view: $view, position: $position, id $id}")
-        }
-
-        btnSearch.setOnClickListener {
-            key = autoTVRoomFind.text.toString()
-            Log.e(TAG, key)
-
-            if (key == "") {
-                showToast("방 제목을 입력해주세요.")
-            } else {
-                val roomName = RoomNameDTO(key)
-                Log.e(TAG, roomName.toString())
-                val call = RetrofitHelper.getApiService().room_search(roomName)
-                call.enqueue(object : Callback<RoomsDTO> {
-                    override fun onResponse(call: Call<RoomsDTO>, response: Response<RoomsDTO>) {
-                        if(response.isSuccessful) {
-                            when (response.code()) {
-                                200 -> {
-                                    val result = response.body()
-                                    for (i: Int in 1..result!!.room.size) {
-                                        val info = result.room[i-1]
-                                        val field = info.field.substring(0, result.room[i-1].field.length -1)
-                                        val fieldArray = field.split(",")
-                                        list.add(RoomModel(info.room_id, info.title, info.maxPeo,
-                                                fieldArray, info.profile))
-                                        Log.e(TAG, "RoomModel(room_id=${info.room_id}, title='${info.title}', maxPeo=${info.maxPeo}," +
-                                                " field='$fieldArray', profile='${info.profile}')")
-
-                                        listRecyclerView()
-                                    }
-                                }
-                                204 -> {
-                                    showToast("해당 제목의 방이 존재하지 않습니다.")
+                                    listRecyclerView()
                                 }
                             }
-                        } else Log.e(TAG+"ERR", "RoomFind통신: ${response.message()}")
-                    }
+                            204 -> {
+                                showToast("해당 제목의 방이 존재하지 않습니다.")
+                            }
+                        }
+                    } else Log.e(TAG+"ERR", "RoomFind통신: ${response.message()}")
+                }
 
-                    override fun onFailure(call: Call<RoomsDTO>, t: Throwable) {
-                        Log.e(TAG+"ERR", "통신안됨: ${t.message}")
-                    }
+                override fun onFailure(call: Call<RoomsDTO>, t: Throwable) {
+                    Log.e(TAG+"ERR", "통신안됨: ${t.message}")
+                }
 
-                })
-            }
+            })
         }
     }
 
     fun listRecyclerView() {
-        val mAdapter = RoomAdapter(applicationContext)
+        val mAdapter = RoomAdapter(this)
         recyclerViewRoomFind.adapter = mAdapter
-        val layoutManager = LinearLayoutManager(applicationContext)
+        val layoutManager = LinearLayoutManager(this)
         recyclerViewRoomFind.layoutManager = layoutManager
         recyclerViewRoomFind.setHasFixedSize(true)
         mAdapter.list = list
