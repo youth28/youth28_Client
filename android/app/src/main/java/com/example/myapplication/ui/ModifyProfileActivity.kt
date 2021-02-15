8ed4f216812e561e319fa8297839af24f61e1eb3
ansuvin <dkstnqls0925@naver.com>
2021-02-15 22:57:42 +0900

    [UPDATE] ModifyProfileActivity

---
+++ android/app/src/main/java/com/example/myapplication/ui/ModifyProfileActivity.kt
@@ -0,0 +1,329 @@
+package com.example.myapplication.ui
+
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.ComponentName
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.myapplication.R
+import com.example.myapplication.api.ApiService
+import com.example.myapplication.databinding.ActivityModifyProfileBinding
+import com.squareup.picasso.Picasso
+import okhttp3.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import java.io.*
+
+class ModifyProfileActivity : AppCompatActivity() {
+
+    val TAG = "ModifyProfile"
+
+    private lateinit var binding: ActivityModifyProfileBinding
+
+    var apiService: ApiService? = null
+    var picUri: Uri? = null
+    private var permissionsToRequest: ArrayList<String>? = null
+    private val permissionsRejected: ArrayList<String> = ArrayList()
+    private val permissions: ArrayList<String> = ArrayList()
+    private val ALL_PERMISSIONS_RESULT = 107
+    private val IMAGE_RESULT = 200
+    var mBitmap: Bitmap? = null
+
+    val btnName = MutableLiveData<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_modify_profile)
+        binding.activity = this
+
+        btnName.observe(this, Observer<String>() {
+            binding.button.text = "${it}"
+        })
+
+        // 퍼미션 요청
+        askPermissions()
+        // 레트로핏 설정
+        initRetrofitClient()
+
+        getImage()
+
+        btnName.value = "다음에 바꾸겠습니다."
+
+       binding.imgProfile.setOnClickListener {
+            startActivityForResult(getPickImageChooserIntent(), IMAGE_RESULT)
+        }
+    }
+
+    fun onBtn() {
+        if (mBitmap != null) {
+            multipartImageUpload()
+        } else {
+            showToast("비트맵 비어있음")
+        }
+    }
+
+    fun getImage() {
+        Picasso.get().load("http://3cccfe4bf700.ngrok.io/uploads/3e11f06c47fd649f31ff123e7bcd57ac.png").into(binding.imgProfile)
+    }
+
+    // region 프로필 사진 서버에 저장하기
+    // 퍼미션 요청
+    private fun askPermissions() {
+        // 외부 저장소 쓰기/읽기
+        permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        // 요청하지 않은 퍼미션 찾아내기
+        permissionsToRequest = findUnAskedPermissions(permissions)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsToRequest!!.size > 0) requestPermissions(
+                    permissionsToRequest!!.toArray(
+                            arrayOfNulls<String>(permissionsToRequest!!.size)
+                    ), ALL_PERMISSIONS_RESULT
+                    // 요청하기
+            )
+        }
+    }
+
+    // 레드로핏 설정
+    private fun initRetrofitClient() {
+        val client = OkHttpClient.Builder().build()
+        apiService =
+                Retrofit.Builder().baseUrl("http://3cccfe4bf700.ngrok.io").client(client).build().create(
+                        ApiService::class.java
+                )
+    }
+
+    // 이미지 선택하기 (가져오기)
+    fun getPickImageChooserIntent(): Intent? {
+        val outputFileUri: Uri = getCaptureImageOutputUri()!!
+        val allIntents: MutableList<Intent> = ArrayList()
+        val packageManager = packageManager
+        // 이미지 선택 Intent
+        val captureIntent = Intent(Intent.ACTION_GET_CONTENT)
+        val listCam = packageManager.queryIntentActivities(captureIntent, 0)
+        for (res in listCam) {
+            val intent = Intent(captureIntent)
+            intent.component = ComponentName(res.activityInfo.packageName, res.activityInfo.name)
+            intent.setPackage(res.activityInfo.packageName)
+            if (outputFileUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+            }
+            allIntents.add(intent)
+        }
+        val galleryIntent = Intent(Intent.ACTION_GET_CONTENT)
+        galleryIntent.type = "image/*"
+        val listGallery = packageManager.queryIntentActivities(galleryIntent, 0)
+        for (res in listGallery) {
+            val intent = Intent(galleryIntent)
+            intent.component = ComponentName(res.activityInfo.packageName, res.activityInfo.name)
+            intent.setPackage(res.activityInfo.packageName)
+            allIntents.add(intent)
+        }
+        var mainIntent = allIntents[allIntents.size - 1]
+        for (intent in allIntents) {
+            if (intent.component!!.className == "com.android.documentsui.DocumentsActivity") {
+                mainIntent = intent
+                break
+            }
+        }
+        allIntents.remove(mainIntent)
+        val chooserIntent = Intent.createChooser(mainIntent, "Select source")
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, allIntents.toTypedArray())
+        return chooserIntent
+    }
+
+    // 이미지 Uri 가져오기
+    private fun getCaptureImageOutputUri(): Uri? {
+        var outputFileUri: Uri? = null
+        val getImage: File? = getExternalFilesDir("")
+        if (getImage != null) {
+            outputFileUri = Uri.fromFile(File(getImage.path, "profile.png"))
+        }
+        return outputFileUri
+    }
+
+    // startActivityForResult 끝나고
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_RESULT) {
+                // 파일 경로 가져오기
+                val filePath = getImageFilePath(data)
+                if (filePath != null) {
+                    // 파일 비트맵으로 만들기
+                    mBitmap = BitmapFactory.decodeFile(filePath)
+                    // 이미지 뷰에 보이기
+                    binding.imgProfile.setImageBitmap(mBitmap)
+                    btnName.value = "이걸로 할게요!!"
+                }
+            }
+        }
+    }
+
+    // 파일경로를 사용하여 이미지 가져오기
+    private fun getImageFromFilePath(data: Intent?): String? {
+        val isCamera = data == null || data.data == null
+        return if (isCamera) getCaptureImageOutputUri()!!.path else getPathFromURI(data!!.data)
+    }
+
+    // 이미지 경로 가져오기
+    fun getImageFilePath(data: Intent?): String? {
+        Log.e(TAG, "getImageFilePath: $data")
+        return getImageFromFilePath(data)
+    }
+
+    // URI를 이용하여 경로 가져오기
+    private fun getPathFromURI(contentUri: Uri?): String? {
+        val proj = arrayOf(MediaStore.Audio.Media.DATA)
+        val cursor: Cursor? = contentResolver.query(contentUri!!, proj, null, null, null)
+        val column_index: Int = cursor!!.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+        cursor.moveToFirst()
+        return cursor.getString(column_index)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("pic_uri", picUri)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        // get the file url
+        picUri = savedInstanceState.getParcelable("pic_uri")
+    }
+
+    // 요청하지 않은 퍼미션 찾아내기
+    private fun findUnAskedPermissions(wanted: ArrayList<String>): ArrayList<String>? {
+        val result = ArrayList<String>()
+        for (perm in wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm)
+            }
+        }
+        return result
+    }
+
+    // 퍼미션이 있는지
+    private fun hasPermission(permission: String): Boolean {
+        if (canMakeSmores()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+        return true
+    }
+
+    private fun showMessageOKCancel(message: String, okListener: DialogInterface.OnClickListener) {
+        AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show()
+    }
+
+    private fun canMakeSmores(): Boolean {
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+
+    // 드디어 올린다 서버에
+    private fun multipartImageUpload() {
+        try {
+            val filesDir: File = applicationContext.filesDir
+            val file = File(filesDir, "image" + ".png")
+            val bos = ByteArrayOutputStream()
+            mBitmap!!.compress(Bitmap.CompressFormat.PNG, 0, bos)
+            val bitmapdata: ByteArray = bos.toByteArray()
+            val fos = FileOutputStream(file)
+            fos.write(bitmapdata)
+            fos.flush()
+            fos.close()
+            val reqFile: RequestBody = RequestBody.create(MediaType.parse("image/*"), file)
+            val body = MultipartBody.Part.createFormData("upload", file.name, reqFile)
+            val name = RequestBody.create(MediaType.parse("text/plain"), "upload")
+            val req: Call<ResponseBody> = apiService!!.postImage(body, name)
+            req.enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                ) {
+                    if (response.code() == 200) {
+                        Log.e("sendImageProfile", "성공입니당")
+                    }
+
+                    showToast(response.code().toString())
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Log.e("sendImageProfile", "실패입니당")
+                    showToast("Request failed")
+                    t.printStackTrace();
+                }
+
+            })
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            ALL_PERMISSIONS_RESULT -> {
+                for (perms in permissionsToRequest!!) {
+                    if (!hasPermission(perms!!)) {
+                        permissionsRejected.add(perms!!)
+                    }
+                }
+                if (permissionsRejected.size > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected[0])) {
+                            showMessageOKCancel(
+                                    "These permissions are mandatory for the application. Please allow access."
+                            ) { dialog, which ->
+                                requestPermissions(
+                                        permissionsRejected.toTypedArray(),
+                                        ALL_PERMISSIONS_RESULT
+                                )
+                            }
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // endregion
+
+    fun showToast(str: String) {
+        Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+    }
+
+}
