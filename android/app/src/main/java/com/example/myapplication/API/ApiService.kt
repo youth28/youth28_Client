f6108021a1fb3b7fe4eb7c81d19ab0b2ea44ccb8
ansuvin <dkstnqls0925@naver.com>
2021-02-16 17:28:24 +0900

    [UPDATE] ImageUpload

--- android/app/src/main/java/com/example/myapplication/API/ApiService.kt
+++ android/app/src/main/java/com/example/myapplication/API/ApiService.kt
@@ -1,19 +1,19 @@
 package com.example.myapplication.api
 
+import com.example.myapplication.dto.UserId
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
 interface ApiService {
 
     @Multipart
-    @POST("/upload")
+    @POST("/image_upload")
     fun postImage(
-            @Part image: MultipartBody.Part,
+            @Part("user_id") userId: UserId,
+            @Part file: MultipartBody.Part,
             @Part("upload") name: RequestBody
     ): Call<ResponseBody>
 }
