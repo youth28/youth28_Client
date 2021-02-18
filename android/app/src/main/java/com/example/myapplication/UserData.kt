5467df473565f007b1925c231403efed06a39f7f
ansuvin <dkstnqls0925@naver.com>
2021-02-18 19:06:56 +0900

    [UPDATE] UserData

--- android/app/src/main/java/com/example/myapplication/UserData.kt
+++ android/app/src/main/java/com/example/myapplication/UserData.kt
@@ -3,13 +3,14 @@ package com.example.myapplication
 class UserData {
     companion object{
         var userId = ""
-        var userpassword = ""
+        var userPassword = ""
         var userNum = ""
         var userName = ""
         var userProfile = ""
+        var userField = ""
 
         fun toStringData(): String {
-            return "UserData{userId='${userId}', userpassword='${userpassword}', userNum='${userNum}', userName='${userName}', userProfile='${userProfile}'}"
+            return "UserData{userId='${userId}', userpassword='${userPassword}', userNum='${userNum}', userName='${userName}', userProfile='${userProfile}', userField='${userField}'}"
         }
     }
 }
