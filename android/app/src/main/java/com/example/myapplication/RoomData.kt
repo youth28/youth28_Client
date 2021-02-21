0e6a3c86ee579fb2fcb416aa6822f5547c80b933
ansuvin <dkstnqls0925@naver.com>
2021-02-21 19:33:51 +0900

    [ADD] RoomData

--- android/app/src/main/java/com/example/myapplication/RoomData.kt
+++ android/app/src/main/java/com/example/myapplication/RoomData.kt
@@ -3,6 +3,10 @@ package com.example.myapplication
 class RoomData {
     companion object{
         var roomId = 0
+        var roomField = ""
+        var maxPeo = 0
+        var profile = ""
+        var title = ""
 
         fun toStringData(): String {
             return "UserData{roomId='${roomId}'}"
