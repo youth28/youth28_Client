392b6784f771ce0d6de41b721ce6854a3469af8b
ansuvin <dkstnqls0925@naver.com>
2021-02-25 19:37:54 +0900

    [UPDATE] CreateVote

--- android/app/src/main/java/com/example/myapplication/dialog/PostVoteContentDialog.kt
+++ android/app/src/main/java/com/example/myapplication/dialog/PostVoteContentDialog.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication.dialog
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.R
+import com.example.myapplication.RoomData
+import com.example.myapplication.UserData
 import com.example.myapplication.api.RetrofitHelper
 import com.example.myapplication.databinding.DialogPostVoteContentBinding
 import com.example.myapplication.dto.*
@@ -22,19 +26,31 @@ class PostVoteContentDialog: DialogFragment() {
     private lateinit var binding: DialogPostVoteContentBinding
 
     // 다이얼로그의 버튼이 눌린경우
-    var listener: (String) -> Unit = {date -> }
+    var listener: (String) -> Unit = {title -> }
 
-    var date = ""
+    var date = MutableLiveData<String>()
     var title = MutableLiveData<String>()
-    val content1 = MutableLiveData<String>()
-    val content2 = MutableLiveData<String>()
-    val content3 = MutableLiveData<String>()
-    val content4 = MutableLiveData<String>()
-    val content5 = MutableLiveData<String>()
 
+    val contentList = arrayListOf(
+            MutableLiveData<String>(), MutableLiveData<String>(), MutableLiveData<String>(), MutableLiveData<String>(), MutableLiveData<String>()
+    )
+
+    var contentCnt = 0
+    var contentCntS = 0
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.dialog_post_vote_content, container, false)
         binding.dialog = this
+
+        binding.apply {
+            date.value = "날짜: ${datePicker.year}-${datePicker.month+1}-${datePicker.dayOfMonth}"
+
+            datePicker.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+                date.value = "날짜: ${year}-${monthOfYear+1}-${dayOfMonth}"
+                invalidateAll()
+            }
+        }
         return binding.root
     }
 
@@ -48,27 +64,57 @@ class PostVoteContentDialog: DialogFragment() {
     }
 
     fun onCreateVote() {
+        RoomData.roomId = 5
+        UserData.userNum = "6"
 
-        /*
-        val voteMakeDTO = VoteMakeDTO(title.value.toString(), content1.value.toString(), content2.value.toString(),
-                content3.value.toString(), content4.value.toString(), content5.value.toString(), date.toString(), "2021-02-27", false, 5, 6)
-        val call = RetrofitHelper.getVoteApi().question_make(voteMakeDTO = voteMakeDTO)
-        call.enqueue(object : Callback<ResponseBody>{
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                if (response.isSuccessful) {
-                    Log.e("response: ", response.code().toString())
-                    listener.invoke(date)
+        contentCnt = 0
+        contentCntS = 0
+
+        if (isNotNull(title.value)) {
+            showToast("투표 제목을 입력해주세요.")
+        } else {
+            for (i: Int in 0..4) {
+                if(!isNotNull(contentList[i].value))
+                    contentCnt ++
+            }
+
+            if(contentCnt < 2) {
+                showToast("선택지를 2개이상 적어주세요")
+            } else {
+                for (i:Int in 0..contentCnt-1) {
+                    if(!isNotNull(contentList[i].value))
+                        contentCntS ++
+                }
+                if (contentCnt != contentCntS) {
+                    showToast("선택지는 앞당겨서 작성해주세요!")
+                } else {
+                    date.value = date.value!!.substring(4, date.value!!.length - 1)
+                    val voteMakeDTO = VoteMakeDTO(title.value.toString(), contentList[0].value.toString(), contentList[1].value.toString(),
+                            contentList[2].value.toString(), contentList[3].value.toString(), contentList[4].value.toString(),
+                            date.value.toString(), "2021-02-27", false, RoomData.roomId, UserData.userNum.toInt())
+                    Log.e("voteMakeDTO", "$voteMakeDTO")
+
+                    listener.invoke(title.value.toString())
                     dismiss()
-                } else Log.e("response: ", "통신 안됨, ${response.message()}")
+
+                    val call = RetrofitHelper.getVoteApi().question_make(voteMakeDTO = voteMakeDTO)
+                    call.enqueue(object : Callback<ResponseBody> {
+                        override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                            if (response.isSuccessful) {
+                                Log.e("response: ", response.code().toString())
+                                listener.invoke(title.value.toString())
+                                dismiss()
+                            } else Log.e("response: ", "통신 안됨, ${response.message()}")
+                        }
+
+                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                            Log.e("onFailure", "${t.message}")
+                        }
+
+                    })
+                }
             }
-
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.e("onFailure", "${t.message}")
-            }
-
-        })
-         */
-
+        }
 /*
         val call2 = RetrofitHelper.getVoteApi().vote(DoVoteDTO("content1", 8, 2))
         call2.enqueue(object : Callback<ResponseBody>{
@@ -159,8 +205,14 @@ class PostVoteContentDialog: DialogFragment() {
          */
     }
 
+    fun isNotNull(value: String?): Boolean {
+        return (value == null || value == "")
+    }
+
 
     fun showToast(str: String) {
         Toast.makeText(context, str, Toast.LENGTH_SHORT).show()
     }
+
 }
+
